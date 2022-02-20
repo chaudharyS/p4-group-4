fe01e2410febb884b778f6631bf91d9cff9aabef
Sanjana Goli <sanjana.goli@gmail.com>
2022-02-20 16:35:11 -0500

    added people icons

--- Refrigeraid/src/components/inventory-tile.js
+++ Refrigeraid/src/components/inventory-tile.js
@@ -7,6 +7,9 @@ import {
   Text,
 } from 'react-native';
 import Cookie from '../assets/foodIcons/cookie.svg';
+import CloseIcon from '../assets/icons/close.svg';
+import Person1 from '../assets/icons/person1.svg';
+import Person2 from '../assets/icons/person2.svg';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
@@ -17,14 +20,21 @@ export default ({
       <View style={styles.container}>
         <View style={styles.imageAndText}>
           <View>
-            <Cookie />
+            <Cookie height={70} width={70} />
           </View>
           <View style={styles.foodDescription}>
             <Text style={styles.foodText}>{foodName}</Text>
             <Text style={styles.purchasedText}>Purchased on [insert date]</Text>
             <Text style={styles.expireText}>Expire: [insert weeks]</Text>
           </View>
-        </View> 
+          <View style={styles.peopleIcons}>
+            <Person1 />
+            <Person2 />
+          </View>
+        </View>
+        <TouchableOpacity style={styles.closeButton}>
+          <CloseIcon height={12} width={12} />
+        </TouchableOpacity>
       </View>
     );
   };
@@ -37,6 +47,17 @@ const styles = StyleSheet.create({
     marginHorizontal: windowWidth * 0.02,
     borderRadius: 20,
     justifyContent: 'center',
+  },
+  closeButton: {
+    position: 'absolute',
+    right: windowWidth * 0.04,
+    top: windowHeight * 0.015,
+  },
+  peopleIcons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginLeft: windowWidth * 0.02,
+    alignContent: 'center',
   },
   foodText: {
     fontFamily: 'SourceSansPro_600SemiBold',
@@ -60,5 +81,6 @@ const styles = StyleSheet.create({
   foodDescription: {
     flexDirection: 'column',
     marginLeft: windowWidth * 0.03,
+    justifyContent: 'space-between',
   }
 });
